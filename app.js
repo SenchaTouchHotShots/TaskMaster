e99a43149defa7cac0aeb30e2145f0aaa88b4fcc
John E. Clark <john@12ftguru.com>
2012-04-19 16:39:31 -0400

    Draft 1 Delivery

--- app.js
+++ app.js
@@ -33,13 +33,14 @@ Ext.application({
     launch: function() {
         var TaskStore = Ext.data.StoreManager.lookup('TaskStore');
         var CompletedStore = Ext.data.StoreManager.lookup('CompletedStore');
+        var today = new Date();
+
         if(CompletedStore.getCount()+TaskStore.getCount() === 0) {
             console.log('we have no records');
-            CompletedStore.add({name: 'Here Is A Task', description: 'You can mark the task complete by clicking the Completed button below.', priority: 1, created: Date.now(), completed: '', isComplete: false});
-            CompletedStore.add({name: 'How To Edit A Task', description: 'You can edit the task by clicking the Edit button below.', priority: 2, created: Date.now(), completed: '', isCompleted: false});
-            CompletedStore.add({name: 'How To Add A Task', description: 'Add a task by clicking the Add button in the upper right corner.', priority: 3, created: Date.now(), completed: '', isComplete: false});
-            CompletedStore.sync();
-            console.log(CompletedStore);
+            TaskStore.add({name: 'Here Is A Task', description: 'You can mark the task complete by clicking the Completed button below.', priority: 1, created: today, completed: '', isComplete: false});
+            TaskStore.add({name: 'How To Edit A Task', description: 'You can edit the task by clicking the Edit button below.', priority: 2, created: today, completed: '', isCompleted: false});
+            TaskStore.add({name: 'How To Add A Task', description: 'Add a task by clicking the Add button in the upper right corner.', priority: 3, created: today, completed: '', isComplete: false});
+            TaskStore.sync();
         } else {
             console.log('we have records');
         }
--- app/view/mainView.js
+++ app/view/mainView.js
@@ -24,10 +24,19 @@ Ext.define('MyApp.view.mainView', {
                 title: 'TaskMaster',
                 items: [
                     {
+                        xtype: 'button',
+                        hidden: true,
+                        id: 'backButton',
+                        itemId: 'mybutton1',
+                        ui: 'back',
+                        text: 'Back'
+                    },
+                    {
                         xtype: 'spacer'
                     },
                     {
                         xtype: 'button',
+                        itemId: 'addButton',
                         text: 'Add'
                     }
                 ]
@@ -37,6 +46,7 @@ Ext.define('MyApp.view.mainView', {
                 layout: {
                     type: 'card'
                 },
+                scrollable: true,
                 title: 'Curent',
                 iconCls: 'organize',
                 items: [
@@ -44,10 +54,13 @@ Ext.define('MyApp.view.mainView', {
                         xtype: 'list',
                         id: 'CurrentList',
                         itemId: 'mylist',
+                        deselectOnContainerClick: false,
                         itemTpl: [
                             '<div class=priority_{priority}>{name}</div>'
                         ],
-                        store: 'TaskStore'
+                        store: 'TaskStore',
+                        onItemDisclosure: false,
+                        preventSelectionOnDisclose: false
                     },
                     {
                         xtype: 'panel',
@@ -56,6 +69,89 @@ Ext.define('MyApp.view.mainView', {
                             '<div class="taskName">{name}</div>',
                             '<div class="taskDescription">{description}</div>',
                             '<div class="taskCreated">Created: {created}</div>'
+                        ],
+                        items: [
+                            {
+                                xtype: 'toolbar',
+                                docked: 'bottom',
+                                items: [
+                                    {
+                                        xtype: 'button',
+                                        id: 'EditButton',
+                                        itemId: 'mybutton4',
+                                        text: 'Edit'
+                                    },
+                                    {
+                                        xtype: 'spacer'
+                                    },
+                                    {
+                                        xtype: 'button',
+                                        id: 'CompleteButton',
+                                        itemId: 'mybutton5',
+                                        text: 'Mark Complete'
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        xtype: 'formpanel',
+                        margin: '',
+                        items: [
+                            {
+                                xtype: 'textfield',
+                                id: 'name',
+                                margin: 3,
+                                label: 'Name',
+                                name: 'name'
+                            },
+                            {
+                                xtype: 'textareafield',
+                                id: 'description',
+                                margin: 3,
+                                label: 'Description',
+                                name: 'description'
+                            },
+                            {
+                                xtype: 'selectfield',
+                                id: 'priority',
+                                margin: 3,
+                                label: 'Priority',
+                                name: 'priority',
+                                value: 2,
+                                hiddenName: 'priority',
+                                options: [
+                                    {
+                                        text: 'High',
+                                        value: 1
+                                    },
+                                    {
+                                        text: 'Medium',
+                                        value: 2
+                                    },
+                                    {
+                                        text: 'Low',
+                                        value: 3
+                                    }
+                                ]
+                            },
+                            {
+                                xtype: 'hiddenfield',
+                                id: 'id',
+                                name: 'id'
+                            },
+                            {
+                                xtype: 'button',
+                                itemId: 'SaveButton',
+                                margin: 10,
+                                text: 'Save'
+                            },
+                            {
+                                xtype: 'button',
+                                itemId: 'CancelButton',
+                                margin: 10,
+                                text: 'Cancel'
+                            }
                         ]
                     }
                 ]
@@ -65,12 +161,14 @@ Ext.define('MyApp.view.mainView', {
                 layout: {
                     type: 'card'
                 },
+                scrollable: true,
                 title: 'Completed',
                 iconCls: 'delete',
                 items: [
                     {
                         xtype: 'list',
                         id: 'CompletedList',
+                        itemId: 'mylist1',
                         itemTpl: [
                             '<div class=priority_{priority}>{name}</div>'
                         ],
@@ -94,23 +192,150 @@ Ext.define('MyApp.view.mainView', {
         },
         listeners: [
             {
+                fn: 'onBackButtonTap',
+                event: 'tap',
+                delegate: '#backButton'
+            },
+            {
+                fn: 'onMybuttonTap',
+                event: 'tap',
+                delegate: '#addButton'
+            },
+            {
                 fn: 'onCurrentListSelect',
                 event: 'select',
                 delegate: '#CurrentList'
+            },
+            {
+                fn: 'onEditButtonTap',
+                event: 'tap',
+                delegate: '#EditButton'
+            },
+            {
+                fn: 'onCompleteButtonTap',
+                event: 'tap',
+                delegate: '#CompleteButton'
+            },
+            {
+                fn: 'onSaveButtonTap',
+                event: 'tap',
+                delegate: '#SaveButton'
+            },
+            {
+                fn: 'onMybutton3Tap',
+                event: 'tap',
+                delegate: '#CancelButton'
+            },
+            {
+                fn: 'onCompletedListSelect',
+                event: 'select',
+                delegate: '#CompletedList'
             }
         ]
     },
 
+    onBackButtonTap: function(button, e, options) {
+        var currentTab = this.getActiveItem();
+        currentTab.setActiveItem(0);
+        button.hide();
+    },
+
+    onMybuttonTap: function(button, e, options) {
+        var tabs = this.getInnerItems();
+        var currentTab = tabs[0];
+        currentTab.setActiveItem(2);
+        this.setActiveItem(currentTab);
+        var backButton = Ext.getCmp('backButton');
+        backButton.hide();
+    },
+
     onCurrentListSelect: function(dataview, record, options) {
         var currentTab = this.getActiveItem();
-        console.log(currentTab);
-        console.log(this);
-
+        var backButton = Ext.getCmp('backButton');
+        backButton.show();
         var currentDetails = currentTab.down('panel');
-        console.log(currentDetails);
 
         currentDetails.setRecord(record);
         currentTab.setActiveItem(currentDetails);
+    },
+
+    onEditButtonTap: function(button, e, options) {
+        var currentTab = this.getActiveItem();
+        var DetailsPanel = currentTab.getActiveItem();
+
+        currentTab.setActiveItem(2);
+        var formPanel = currentTab.getActiveItem();
+        formPanel.setRecord(DetailsPanel.getRecord());
+
+        this.setActiveItem(currentTab);
+
+        var backButton = Ext.getCmp('backButton');
+        backButton.hide();
+    },
+
+    onCompleteButtonTap: function(button, e, options) {
+        var currentTab = this.getActiveItem();
+        var detailsPanel = currentTab.getActiveItem();
+
+        var record = detailsPanel.getRecord();
+
+        record.set('completed', new Date());
+        record.set('isComplete', true);
+
+        var store = Ext.data.StoreManager.lookup('TaskStore');
+        store.sync();
+
+        this.setActiveItem(1);
+        var completedList = this.getActiveItem();
+        var completedStore = completedList.getActiveItem().getStore();
+        completedStore.add(record);
+        completedList.getActiveItem().refresh();
+
+        currentTab.setActiveItem(0);
+
+        var backButton = Ext.getCmp('backButton');
+        backButton.hide();
+    },
+
+    onSaveButtonTap: function(button, e, options) {
+        var currentTab = this.getActiveItem();
+        var formPanel = currentTab.getActiveItem();
+
+        var values = formPanel.getValues();
+
+        var store = Ext.data.StoreManager.lookup('TaskStore');
+        console.log(values.id);
+        if(values.id === null) {
+            console.log('got NO ID');
+            var record = Ext.ModelMgr.create(values, 'MyApp.model.Task');
+            record.set('created', new Date());
+            store.add(record);
+        } else {
+            console.log('got an ID');
+            var record = store.getById(values.id);
+            record.set('name', values.name);
+            record.set('description', values.description);
+            record.set('priority', values.priority);
+        }
+
+        store.sync();
+        formPanel.reset();
+        currentTab.setActiveItem(0);
+    },
+
+    onMybutton3Tap: function(button, e, options) {
+        var currentTab = this.getActiveItem();
+        currentTab.setActiveItem(0);
+    },
+
+    onCompletedListSelect: function(dataview, record, options) {
+        var completedTab = this.getActiveItem();
+        var backButton = Ext.getCmp('backButton');
+        backButton.show();
+        var completedDetails = completedTab.down('panel');
+
+        completedDetails.setRecord(record);
+        completedTab.setActiveItem(completedDetails);
     }
 
 });
